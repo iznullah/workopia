dd9c81eb14a729b5648aa2a0c6ff31138a3d82d9
Iznullah <iznullam@gmail.com>
2024-12-27 16:31:34 +0300

    Workopia

---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+<h1>Available jobs</h1>
+<ul>
+    @forelse($jobs as $job)
+
+        <li>{{$job}}</li>
+
+    @empty
+        <li>No jobs available</li>
+    @endforelse
+</ul>
+</x-layout>
---
+++ resources/views/jobs/show.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Show Jobs!</h1>
+</x-layout>
