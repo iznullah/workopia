4ace20ccaa3370986c02a5a979f062b9ef4ed5f6
Iznullah <iznullam@gmail.com>
2025-01-01 20:10:05 +0300

    Workopia

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
+    use HasFactory;
     protected $table = 'job_listings';
+    protected $fillable = ['user_id', 'title', 'description', 'salary',
+        'tags', 'job_type', 'remote',
+        'address', 'city', 'state', 'zip-code', 'contact-email',
+        'contact-phone', 'company-name', 'company-description',
+        'company-logo', 'company-website'];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
